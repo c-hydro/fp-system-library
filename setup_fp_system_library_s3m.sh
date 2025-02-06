a23fdb94cacc0c83198177e4209096a7688060b1
Fabio Delogu <fabio.delogu@cimafoundation.org>
2025-02-06 15:35:33 +0100

    Update setup_fp_system_library_s3m.sh
    bug fix related to zlib library

--- setup_fp_system_library_s3m.sh
+++ setup_fp_system_library_s3m.sh
@@ -18,7 +18,7 @@ script_version="1.0.0"
 script_date='2022/11/17'
 
 # Define file reference path according with https link(s)
-fileref_zlib='http://www.zlib.net/zlib-1.2.13.tar.gz'
+fileref_zlib='http://www.zlib.net/zlib-1.3.1.tar.gz'
 fileref_hdf5='https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8/hdf5-1.8.17/src/hdf5-1.8.17.tar.gz'
 fileref_nc4_c='https://github.com/Unidata/netcdf-c/archive/refs/tags/v4.6.0.tar.gz'
 fileref_nc4_fortran='https://github.com/Unidata/netcdf-fortran/archive/refs/tags/v4.4.3.tar.gz'
